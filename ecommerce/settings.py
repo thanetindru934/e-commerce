f66d5515dd409de8aedb0c3011f81f0c72d0484b
Indra Thanet <thanetindru934@gmail.com>
2026-09-08 10:22:55 +0930

    Fix product images

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -137,6 +137,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn","static_root")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn","media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGOUT_REDIRECT_URL = "/login/"
